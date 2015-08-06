5758857491a548de34ef618ddf31675cfdd29519
shrimp78 <y.kitamura78@gmail.com>
2015-08-06 20:28:18 +0900

    9.0

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,6 +6,7 @@ describe "Authentication" do
 
   describe "signin" do
     before { visit signin_path }
+
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before do
@@ -18,6 +19,13 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
     end
   end
 end
